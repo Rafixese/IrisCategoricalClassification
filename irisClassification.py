9b542f465ec6945e0d97092eea725c32ef8b1ff9
Rafał Hrabia <hrabia.rafal98@gmail.com>
2020-04-15 17:48:19 +0200

    Added data preprocessing, added KNeighborsClassifier

---
+++ irisClassification.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 16:33:41 2020
+
+@author: ganja
+"""
+#%%########
+# Imports #
+###########
+
+import pandas as pd
+
+
+
+#%%###################
+# Data preprocessing #
+######################
+
